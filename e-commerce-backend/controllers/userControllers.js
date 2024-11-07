43ab384211c6e6f482259f0f0a3d6d536288a148
Blessiey <mogashoablessing676@gmail.com>
2024-11-07 15:25:50 +0200

    Commit

--- e-commerce-backend/controllers/userControllers.js
+++ e-commerce-backend/controllers/userControllers.js
@@ -1,11 +1,27 @@
 import asyncHandler from "express-async-handler";
+import User from "../models/userModels.js";
+import generateToken from "../utils/generateToken.js";
 
 // @desc         Auth user/set token
 // route         POST /api/users/auth
 // @access       Public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Auth User" });
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 // @desc         Register a new User
@@ -13,14 +29,44 @@ const authUser = asyncHandler(async (req, res) => {
 // @access       Public
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User Already Exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 // @desc         Logout User
 // route         POST /api/users/logout
 // @access       Public
 
 const logoutUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Logout User" });
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "User Logged Out" });
 });
 
 // @desc         Get user Profile
@@ -28,14 +74,37 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @access       Public
 
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 // @desc         Get user Profile
 // route         PUT /api/users/profile
 // @access       Public
 
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Update User Profile" });
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
 });
 
 export {
--- e-commerce-backend/server.js
+++ e-commerce-backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import { errorHandler, notFound } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 5000;
 
@@ -10,6 +11,11 @@ dotenv.config();
 connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send("Server Connected"));
 
